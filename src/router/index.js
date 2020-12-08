cb6f132c790484472f3867424d01845bd33974f9
tuanconvmodev <tuan.khuat@vmodev.com>
2020-12-08 14:08:36 +0700

    [TuanKa] remove command

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,31 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-const Home = () => import(/* webpackChunkName: "vsf-home" */ '@/pages/home')
-const Profile = () => import(/* webpackChunkName: "vsf-home" */ '@/pages/profile')
-const Page404 = () => import(/* webpackChunkName: "vsf-home" */ '@/pages/404')
-const PageAuth = () => import(/* webpackChunkName: "vsf-home" */ '@/pages/auth')
-
-
+import Vue from "vue";
+import VueRouter from "vue-router";
+const Home = () => import(/* webpackChunkName:*/ "@/pages/home");
+const Profile = () => import(/* webpackChunkName:  */ "@/pages/profile");
+const Page404 = () => import(/* webpackChunkName: */ "@/pages/404");
+const PageAuth = () => import(/* webpackChunkName:  */ "@/pages/auth");
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'Home',
-            component: Home
-        },
-        {
-            path: '/profile',
-            name: 'Profile',
-            component: Profile
-        },
-        {
-            path: '/login',
-            name: 'PageAuth',
-            component: PageAuth
-        },
-        { path: '/404', component: Page404 },
-        { path: '*', redirect: '/404' },
-    ]
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home,
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+      component: Profile,
+    },
+    {
+      path: "/login",
+      name: "PageAuth",
+      component: PageAuth,
+    },
+    { path: "/404", component: Page404 },
+    { path: "*", redirect: "/404" },
+  ],
 });
