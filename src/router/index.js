73e734d11b64f7b319804156bf8175e56f5ee56b
tuanconvmodev <tuan.khuat@vmodev.com>
2020-12-16 13:46:41 +0700

    change name pages ->views

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-const Home = () => import(/* webpackChunkName: "" */ '@/pages/home');
-const Profile = () => import(/* webpackChunkName: "" */ '@/pages/profile');
-const Page404 = () => import(/* webpackChunkName: "" */ '@/pages/404');
-const PageAuth = () => import(/* webpackChunkName: "" */ '@/pages/auth');
+const Home = () => import(/* webpackChunkName: "" */ '@/views/home');
+const Profile = () => import(/* webpackChunkName: "" */ '@/views/profile');
+const Page404 = () => import(/* webpackChunkName: "" */ '@/views/404');
+const PageAuth = () => import(/* webpackChunkName: "" */ '@/views/auth');
 
 Vue.use(VueRouter);
 
