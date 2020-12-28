88612169544a247c6d9c20a72d535423c84654a7
dat26061994 <dat26061994@gmail.com>
2020-12-28 15:44:39 +0700

    skeleton loaders

---
+++ src/components/SkeletonLoaders.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-skeleton-loader :class="classProps" :dark="true" :type="type"></v-skeleton-loader>
+</template>
+
+<script>
+export default {
+  props: {
+    /* type of skeleton loader:
+        card-avatar,
+        date-picker,
+        article,
+        actions,
+        table-heading,
+        list-item-two-line,
+        image,
+        table-tfoot,
+        list-item-avatar,
+        divider,
+        list-item-three-line,
+        card-heading,
+        actions
+    */
+    type: {
+      type: String,
+      default: 'card-avatar, article, actions',
+    },
+    classProps: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
