10c4d19b46b10c3dfdfce7c98435c2ce1865b197
tuanconvmodev <tuan.khuat@vmodev.com>
2020-12-28 15:24:22 +0700

    add test component

--- src/const/mockData.js
+++ src/const/mockData.js
@@ -1,24 +1,25 @@
-/* eslint-disable prettier/prettier */
 const menu = [
-    {
-        title: 'Home',
-        icon: 'mdi-folder',
-        path: '/w/abc.hiwebio/',
-    },
-    {
-        title: 'Products',
-        icon: 'mdi-folder',
+  {
+    title: 'Home',
+    icon: 'mdi-folder',
+    path: '/w/abc.hiwebio/',
+  },
+  {
+    title: 'Products',
+    icon: 'mdi-folder',
+    path: '/w/abc.hiwebio/products',
+    children: [
+      {
+        title: 'All products',
         path: '/w/abc.hiwebio/products',
-        children: [
-            {
-                title: 'All products',
-                path: '/w/abc.hiwebio/products',
-            },
-
-        ],
-    },
-
+      },
+      {
+        title: 'Test componet',
+        path: '/w/abc.hiwebio/dattest',
+      },
+    ],
+  },
 ];
 export default {
-    menu,
+  menu,
 };
---
+++ src/views/website/test/DAT.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>anh tesst component ow day</div>
+</template>
+
+<script>
+export default {};
+</script>
