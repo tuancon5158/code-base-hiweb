8c8f5c63ae9461ea4382e656f8a20b7556407332
dat26061994 <dat26061994@gmail.com>
2020-12-30 18:30:09 +0700

    tags field

---
+++ src/components/form/TagsField.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container fluid>
+    <v-combobox
+      v-model="$parent[model]"
+      hide-selected
+      hint="Maximum of 5 tags"
+      multiple
+      persistent-hint
+      small-chips
+      @input="inputChange"
+    >
+    </v-combobox>
+  </v-container>
+</template>
+
+<script>
+import { required } from 'vuelidate/lib/validators';
+
+export default {
+  props: {
+    model: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    inputChange() {
+      console.log(this.$v);
+    },
+  },
+  validations: {
+    model: {
+      required,
+    },
+  },
+};
+</script>
